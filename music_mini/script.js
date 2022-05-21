968f6beb249b78f9f6cb8582d5aa0a1d112542f9
F-gen <1101853378@qq.com>
2022-05-21 23:33:04 +0800

    music

---
+++ music_mini/script.js
@@ -0,0 +1,14 @@
+const musicContainer=document.querySelector('music-container')
+const playbtn=document.querySelector('#play')
+const prevbtn=document.querySelector('#prev')
+const nextbtn=document.querySelector('#next')
+const audio=document.querySelector('#audio')
+const progress=document.querySelector('.progress')
+const progressContainer=document.querySelector('.progress-container')
+const title=document.querySelector('#title')
+const cover=document.querySelector('#cover')
+
+//song tiles
+const songs=["hey","summer","ukulele"]
+
+// keep track of song
